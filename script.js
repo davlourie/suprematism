4092c89b0ab104cdaaecdb8830df2f328be41af0
davlourie <jour.david.arakelyan@gmail.com>
2025-05-14 15:30:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.querySelectorAll(".moveable").forEach((el) => {
-  el.addEventListener("click", () => {
-    const canvas = document.querySelector(".canvas");
+document.querySelectorAll('.moveable').forEach(el => {
+  el.addEventListener('click', () => {
+    const canvas = document.querySelector('.canvas');
     const canvasWidth = canvas.clientWidth;
     const canvasHeight = canvas.clientHeight;
     const elWidth = el.offsetWidth;
@@ -9,10 +9,13 @@ document.querySelectorAll(".moveable").forEach((el) => {
     const maxLeft = canvasWidth - elWidth;
     const maxTop = canvasHeight - elHeight;
 
-    const newLeft = Math.floor(Math.random() * maxLeft);
-    const newTop = Math.floor(Math.random() * maxTop);
+    const newLeft = Math.floor(Math.random() * maxLeft) + elWidth / 2;
+    const newTop = Math.floor(Math.random() * maxTop) + elHeight / 2;
 
-    el.style.left = `${newLeft}px`;
-    el.style.top = `${newTop}px`;
+    const percentLeft = (newLeft / canvasWidth) * 100;
+    const percentTop = (newTop / canvasHeight) * 100;
+
+    el.style.left = `${percentLeft}%`;
+    el.style.top = `${percentTop}%`;
   });
 });
